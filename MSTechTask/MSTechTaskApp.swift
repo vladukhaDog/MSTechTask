0e021b633b53bfe63c551fb003aef588ef442769
vladukha <vladic.p@gmail.com>
2026-01-14 18:18:45 +0300

    Initial Commit

---
+++ MSTechTask/MSTechTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSTechTaskApp.swift
+//  MSTechTask
+//
+//  Created by Владислав Пермяков on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MSTechTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
